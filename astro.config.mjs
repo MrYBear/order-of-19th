bea921261d34b136ff1d7552f7f4c6fcd26b85d0
Matt <mattcons@gmail.com>
2026-01-02 17:34:59 +0100

    stict update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 // @ts-ignore - Tailwind Vite plugin types
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://OrderXIX.com',
+  output: 'static',
+  adapter: netlify(),
   integrations: [
     mdx(), 
     sitemap()
